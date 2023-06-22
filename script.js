9af215db17ce3e5027b44fe801811995e20b2f09
jayakrishnapalla05 <130897074+jayakrishnapalla05@users.noreply.github.com>
2023-06-22 12:59:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const sizeInfo=document.querySelector("#sizeInfo");
+const width=document.querySelector("h1:first-child");
+const height=document.querySelector("h1:last-child");
+
+function screensize(){
+	width.innerText='Width: ${window.innerWidth}px';
+	height.innerText='Height: ${window.innerHeight}px';
+	
+}
+window.addEventListener('resize', screensize);
+screensize();
